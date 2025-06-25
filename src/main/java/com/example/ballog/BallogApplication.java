fa9333f8c7ff82020c5f0df4211f17fc85000daf
inha0319@naver.com <inha0319@naver.com>
2025-06-25 14:55:59 +0900

    chore : 프로젝트 초기세팅

---
+++ src/main/java/com/example/ballog/BallogApplication.java
@@ -0,0 +1,13 @@
+package com.example.ballog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BallogApplication.class, args);
+    }
+
+}
