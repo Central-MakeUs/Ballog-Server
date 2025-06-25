b4ec677833c330a13f3f1282ed5d5790d8fd1000
inha0319@naver.com <inha0319@naver.com>
2025-06-25 14:55:59 +0900

    chore : 프로젝트 초기세팅

---
+++ src/test/java/com/example/ballog/BallogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ballog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
