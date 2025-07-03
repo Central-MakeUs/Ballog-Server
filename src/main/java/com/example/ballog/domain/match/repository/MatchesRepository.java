9179f92bad1ed07a53911a2880f0c840cd70e658
inha0319@naver.com <inha0319@naver.com>
2025-07-03 16:15:15 +0900

    feat : 당일 야구 경기 조회 기능

--- src/main/java/com/example/ballog/domain/match/repository/MatchesRepository.java
+++ src/main/java/com/example/ballog/domain/match/repository/MatchesRepository.java
@@ -4,6 +4,10 @@ import com.example.ballog.domain.match.entity.Matches;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface MatchesRepository extends JpaRepository<Matches, Long> {
+    List<Matches> findAllByMatchesDate(LocalDate date);
 }
