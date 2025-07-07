623daf53f70818818b09c27ffa145926cf52fb46
inha0319@naver.com <inha0319@naver.com>
2025-07-07 19:57:44 +0900

    feat : 직관기록 전체 조회 기능 구현

--- src/main/java/com/example/ballog/domain/matchrecord/repository/MatchRecordRepository.java
+++ src/main/java/com/example/ballog/domain/matchrecord/repository/MatchRecordRepository.java
@@ -11,4 +11,5 @@ import java.util.List;
 public interface MatchRecordRepository extends JpaRepository<MatchRecord, Long> {
     long countByUser(User user);
     List<MatchRecord> findAllByResultIsNull();
+    List<MatchRecord> findAllByUserOrderByMatchrecordIdDesc(User user);
 }
