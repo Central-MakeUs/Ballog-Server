dfc24658079291f2c0e8065d24466f7650560169
inha0319@naver.com <inha0319@naver.com>
2025-07-08 22:49:42 +0900

    feat : 감정표현 기능 구현

---
+++ src/main/java/com/example/ballog/domain/emotion/dto/request/EmotionRequest.java
@@ -0,0 +1,16 @@
+package com.example.ballog.domain.emotion.dto.request;
+
+import com.example.ballog.domain.emotion.entity.EmotionType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmotionRequest {
+    private Long recordId;
+    private EmotionType emotionType;
+}
