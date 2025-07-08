50a0da05f1e67de6d53a9627eccfccdcceffe487
inha0319@naver.com <inha0319@naver.com>
2025-07-08 23:22:21 +0900

    feat : 감정표현 조회

--- src/main/java/com/example/ballog/domain/emotion/repository/EmotionRepository.java
+++ src/main/java/com/example/ballog/domain/emotion/repository/EmotionRepository.java
@@ -1,7 +1,11 @@
 package com.example.ballog.domain.emotion.repository;
 
 import com.example.ballog.domain.emotion.entity.Emotion;
+import com.example.ballog.domain.matchrecord.entity.MatchRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmotionRepository extends JpaRepository<Emotion, Long> {
+    List<Emotion> findByMatchRecord(MatchRecord matchRecord);
 }
