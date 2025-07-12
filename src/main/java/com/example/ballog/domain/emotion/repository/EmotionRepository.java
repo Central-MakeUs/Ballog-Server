53d870655c707fde983f7d067b16f23c51f687a8
inha0319@naver.com <inha0319@naver.com>
2025-07-12 13:06:45 +0900

    fix : 충돌 해결

--- src/main/java/com/example/ballog/domain/emotion/repository/EmotionRepository.java
+++ src/main/java/com/example/ballog/domain/emotion/repository/EmotionRepository.java
@@ -5,17 +5,11 @@ import com.example.ballog.domain.matchrecord.entity.MatchRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-=======
-
-import java.util.List;
-
->>>>>>> origin/develop
 public interface EmotionRepository extends JpaRepository<Emotion, Long> {
     List<Emotion> findByMatchRecord(MatchRecord matchRecord);
     @Query("SELECT e FROM Emotion e WHERE e.matchRecord.matchrecordId = :recordId")
