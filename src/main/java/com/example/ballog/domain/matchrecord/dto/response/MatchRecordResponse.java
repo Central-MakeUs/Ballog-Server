69beb3df896cf0fe544ee48b5bd873e641b64a14
inha0319@naver.com <inha0319@naver.com>
2025-07-23 17:28:10 +0900

    feat : 직관기록-response 정보 추가

--- src/main/java/com/example/ballog/domain/matchrecord/dto/response/MatchRecordResponse.java
+++ src/main/java/com/example/ballog/domain/matchrecord/dto/response/MatchRecordResponse.java
@@ -5,16 +5,18 @@ import com.example.ballog.domain.login.entity.BaseballTeam;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Getter
 @Builder
 public class MatchRecordResponse {
     private Long matchRecordId;
-
     private Long matchesId;
-    private String homeTeam;
-    private String awayTeam;
-    private String matchDate;
-    private String matchTime;
+    private BaseballTeam homeTeam;
+    private BaseballTeam awayTeam;
+    private LocalDate matchDate;
+    private LocalTime matchTime;
     private Long userId;
     private Long watchCnt;
     private Result result;
