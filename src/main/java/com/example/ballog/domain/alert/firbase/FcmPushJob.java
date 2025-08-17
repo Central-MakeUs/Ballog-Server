2391d354a40f8b380c2dee87833aa6e36143c07d
inha0319@naver.com <inha0319@naver.com>
2025-08-17 09:51:19 +0900

    feat : fcm 코드 수정

--- src/main/java/com/example/ballog/domain/alert/firbase/FcmPushJob.java
+++ src/main/java/com/example/ballog/domain/alert/firbase/FcmPushJob.java
@@ -3,7 +3,9 @@ package com.example.ballog.domain.alert.firbase;
 import com.example.ballog.domain.alert.dto.request.FcmMessageRequest;
 import com.example.ballog.domain.alert.entity.Alert;
 import com.example.ballog.domain.alert.repository.AlertRepository;
+import com.example.ballog.domain.login.entity.FcmToken;
 import com.example.ballog.domain.login.entity.OAuthToken;
+import com.example.ballog.domain.login.repository.FcmTokenRepository;
 import com.example.ballog.domain.login.repository.OAuthTokenRepository;
 import com.example.ballog.domain.login.repository.UserRepository;
 import com.example.ballog.domain.login.entity.User;
@@ -24,7 +26,7 @@ public class FcmPushJob implements Job {
     private UserRepository userRepository;
 
     @Autowired
-    private OAuthTokenRepository oAuthTokenRepository;
+    private FcmTokenRepository fcmTokenRepository;
 
     @Autowired
     private AlertRepository alertRepository;
@@ -45,8 +47,8 @@ public class FcmPushJob implements Job {
         if (type.equals("start_alert") && !alert.getStartAlert()) return;
         if (type.equals("in_game_alert") && !alert.getInGameAlert()) return;
 
-        OAuthToken fcmToken = oAuthTokenRepository.findByUserAndProvider(user, "fcm").orElse(null);
-        if (fcmToken == null || fcmToken.getAccessToken() == null) return;
+        FcmToken fcmToken = fcmTokenRepository.findByUser(user).orElse(null);
+        if (fcmToken == null || fcmToken.getDeviceToken() == null) return;
 
         String title = type.equals("start_alert") ? teamName + " 경기 임박!" : teamName + " 경기 중!";
         String body = type.equals("start_alert")
@@ -54,7 +56,7 @@ public class FcmPushJob implements Job {
                 : "지금 그 순간, 볼로그에 남겨볼까요?";
 
         FcmMessageRequest.NotificationDto notification = new FcmMessageRequest.NotificationDto(title, body);
-        FcmMessageRequest request = new FcmMessageRequest(fcmToken.getAccessToken(), notification);
+        FcmMessageRequest request = new FcmMessageRequest(fcmToken.getDeviceToken(), notification);
         firebaseMessageService.sendMessage(request);
     }
 }
