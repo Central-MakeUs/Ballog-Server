783356e0eb7e51fc44a9bfaff766cb0cd93c44bb
inha0319@naver.com <inha0319@naver.com>
2025-10-02 20:14:11 +0900

    refactor : SwaggerConfig Authorization 추가

--- src/main/java/com/example/ballog/global/config/SwaggerConfig.java
+++ src/main/java/com/example/ballog/global/config/SwaggerConfig.java
@@ -1,18 +1,24 @@
 package com.example.ballog.global.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public GroupedOpenApi jwtApi(@Qualifier("apiErrorExampleCustomizer")OperationCustomizer apiErrorExampleCustomizer){
+    public GroupedOpenApi jwtApi(@Qualifier("apiErrorExampleCustomizer") OperationCustomizer apiErrorExampleCustomizer){
         return GroupedOpenApi.builder()
                 .group("jwt-api")
                 .pathsToMatch("/**")
@@ -21,12 +27,27 @@ public class SwaggerConfig {
     }
 
     @Bean
-    public OpenAPI customOpenAPI(){
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(new Info().title("Ballog API")
-                        .description("Ballog API 입니다")
-                        .version("v1.0.0"));
-    }
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("로컬 개발 서버"),
+                        new Server().url("https://ballog.shop").description("운영 - API 서버")
+                ))
 
+                .components(
+                        new Components()
+                                .addSecuritySchemes("bearerAuth",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .info(new Info()
+                        .title("Ballog API")
+                        .description("Ballog URL API 명세서입니다")
+                        .version("v1.0.0")
+                );
+    }
 }
